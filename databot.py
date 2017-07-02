db71913df444a07e6a84f931181d8e79b5aa76f3
Cameron <gillespcam@gmail.com>
2017-07-02 20:46:06 +1200

    Count persistence
    saves user data!
    the file size could get kinda large but idk
    I need to add comments later

--- databot.py
+++ databot.py
@@ -2,9 +2,17 @@ import discord
 from discord.ext import commands
 import asyncio
 import datetime
+import pickle
 
-countcommand= "!msgcount" #choose a command to activate count
-token =  "" #token goes here
+
+try:
+    msgData = pickle.load(open('msgData.obj', 'rb')) 
+except:
+    msgData = [[],[]]
+    
+countcommand= "!msgtotal" #choose a command to activate count
+trackcommand = "!msgtrack" #the command to start tracking a user
+token = "MzI5NTAwNzk4Njg3OTAzNzQ4.DDmBDQ.KnKS_TYWfFvsDJ579GyjjRMQCMk" #token goes here
 
 client = discord.Client()
 bot = commands.Bot(command_prefix='?', description='A simple discord python bot')
@@ -35,6 +43,11 @@ async def on_message(message):
                 for n in message.mentions:
                     await countmessages(n, message.author, message.channel_mentions, message) #call the count message function
 
+    elif message.content.startswith(trackcommand):
+        await client.delete_message(message)
+        await trackmessages(message)
+        
+    
     elif message.content.startswith('!sleep'):
         await client.delete_message(message)
         await asyncio.sleep(5)
@@ -52,5 +65,63 @@ async def countmessages(user, mauthor, SearchChannel, message): #function to ret
     uptime = end_time - start_time
     print("Retrieved {}'s data for {}, in {}. Took {}.".format(user.name, message.author.name, message.server.name, uptime))
     await client.edit_message(tmp, '{} has posted {} messages. Took {} seconds.'.format(user.display_name, counter, uptime))
+
+async def trackmessages(message):
+    start_time = datetime.datetime.now()
+    user = message.author
+    server = message.server
+    sid = server.id
+    uid = user.id
+    newuser = False;
+
+    tmp = await client.send_message(message.channel, "Counting {}'s messages...".format(user.display_name))#temp message
+    if sid in msgData[0]:  #If the server is already loaded
+        sindex = msgData[0].index(sid)
+        if uid in msgData[1][sindex][0]:  #if the user already has data in that server
+            print ("Retrieving data for {} in server {}.".format(user.name, message.server.name))
+            uindex = msgData[1][sindex][0].index(uid)
+
+        else:
+            sindex = msgData[0].index(sid)
+            msgData[1][sindex][0].append(uid)
+            uindex = msgData[1][sindex][0].index(uid)
+            userData = [[] for i in range(2)]
+            msgData[1][sindex][1][uindex] = userData
+            newuser = True
+            print ("Getting new data for {} in server {}.".format(user.name, message.server.name))
+
+    else:
+        try:
+            print ("Getting new data for {} in new server {}.".format(user.name, message.server.name))
+            msgData[0].append(sid)
+            sindex = msgData[0].index(sid)
+            msgData[1].append([[],[]])
+            msgData[1][sindex][0].append(uid)
+            uindex = msgData[1][sindex][0].index(uid)
+            userData = [[] for i in range(2)]
+            msgData[1][sindex][1].append(userData)
+            newuser = True
+        except:
+            print ('Something failed lol')
+            
+    if newuser == True:        
+        for channel in server.channels:
+            async for log in client.logs_from(channel, limit=100000):
+                if log.author == user:
+                    msgData[1][sindex][1][uindex][1].append([message.channel, message.timestamp])
+    else:
+        lastmsgtimestamp = msgData[1][sindex][1][uindex][1][-1][1]
+        for channel in server.channels:
+            async for log in client.logs_from(channel, limit=100000, after = lastmsgtimestamp):
+                if log.author == user:
+                    msgData[1][sindex][1][uindex][1].append([message.channel, message.timestamp])
+
+    userMessages = len(msgData[1][sindex][1][uindex][1])
+    end_time = datetime.datetime.now()
+    uptime = end_time - start_time
+    await client.edit_message(tmp, '{} has posted {} messages. Took {} seconds.'.format(user.display_name, userMessages, uptime))
+
+    file_pi = open('msgData.obj', 'wb')
+    pickle.dump(msgData, file_pi) 
 
 client.run(token)
