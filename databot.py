e386e0d39e4b2c6e68664f540c05521313504115
Cameron <gillespcam@gmail.com>
2017-07-03 05:58:02 +1200

    changes, error fixing
    catches some errors gracefully now.

--- databot.py
+++ databot.py
@@ -6,13 +6,13 @@ import pickle
 
 
 try:
-    msgData = pickle.load(open('msgData.obj', 'rb')) 
+    msgData = pickle.load(open('msgData.dat', 'rb')) 
 except:
     msgData = [[],[]]
     
-countcommand= "!msgtotal" #choose a command to activate count
+countcommand= "!msgcount" #choose a command to activate count
 trackcommand = "!msgtrack" #the command to start tracking a user
-token = "MzI5NTAwNzk4Njg3OTAzNzQ4.DDmBDQ.KnKS_TYWfFvsDJ579GyjjRMQCMk" #token goes here
+token = "" #token goes here
 
 client = discord.Client()
 bot = commands.Bot(command_prefix='?', description='A simple discord python bot')
@@ -24,32 +24,51 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-
+    await client.change_presence(game=discord.Game(name='use {} or {}'.format(trackcommand, countcommand)))
+    
 @client.event
 async def on_message(message):
     if message.content.startswith(countcommand):
-        print("{} requested data in {}.".format(message.author.name, message.server.name))
-        await client.delete_message(message)
-
-        if len(message.mentions) == 0 and len(message.channel_mentions) == 0:
-            await countmessages(message.author, message.author, message.server.channels, message)
+        if message.channel.is_private:
+                tmp = await client.send_message(message.channel, 'Sorry, this function is not available in private messages.')
         else:
-            if len(message.channel_mentions) == 0:
-                for n in message.mentions:
-                    await countmessages(n, message.author, message.server.channels, message)
-            elif len(message.mentions) == 0:
-                await countmessages(message.author, message.author, message.channel_mentions, message)
+            print("{} requested data in {}.".format(message.author.name, message.server.name))
+
+            try:
+                await client.delete_message(message)
+            except:
+                print ('Error deleting message')
+        
+
+            if len(message.mentions) == 0 and len(message.channel_mentions) == 0:
+                await countmessages(message.author, message.author, message.server.channels, message)
             else:
-                for n in message.mentions:
-                    await countmessages(n, message.author, message.channel_mentions, message) #call the count message function
+                if len(message.channel_mentions) == 0:
+                    for n in message.mentions:
+                        await countmessages(n, message.author, message.server.channels, message)
+                elif len(message.mentions) == 0:
+                    await countmessages(message.author, message.author, message.channel_mentions, message)
+                else:
+                    for n in message.mentions:
+                        await countmessages(n, message.author, message.channel_mentions, message) #call the count message function
 
     elif message.content.startswith(trackcommand):
-        await client.delete_message(message)
-        await trackmessages(message)
+        if message.channel.is_private:
+                tmp = await client.send_message(message.channel, 'Sorry, this function is not available in private messages.')
+        else:
+            try:
+            
+                await client.delete_message(message)
+            except:
+                print ('Error deleting message')
+            await trackmessages(message)
         
     
     elif message.content.startswith('!sleep'):
-        await client.delete_message(message)
+        try:
+            await client.delete_message(message)
+        except:
+            print ('Error deleting message')
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'Done sleeping')
 
@@ -58,9 +77,12 @@ async def countmessages(user, mauthor, SearchChannel, message): #function to ret
     start_time = datetime.datetime.now()
     tmp = await client.send_message(message.channel, "Counting {}'s messages...".format(user.display_name))#temp message
     for channel in SearchChannel:
-        async for log in client.logs_from(channel, limit=50000):
-            if log.author == user:
-                counter += 1
+        try:
+            async for log in client.logs_from(channel, limit=50000):
+                if log.author == user:
+                    counter += 1
+        except:
+            print ("Forbidden error. Probably a nsfw channel.")
     end_time = datetime.datetime.now()
     uptime = end_time - start_time
     print("Retrieved {}'s data for {}, in {}. Took {}.".format(user.name, message.author.name, message.server.name, uptime))
@@ -85,8 +107,8 @@ async def trackmessages(message):
             sindex = msgData[0].index(sid)
             msgData[1][sindex][0].append(uid)
             uindex = msgData[1][sindex][0].index(uid)
-            userData = [[] for i in range(2)]
-            msgData[1][sindex][1][uindex] = userData
+            userData = [[],[],[0,datetime.datetime.now()]]
+            msgData[1][sindex][1].append(userData)
             newuser = True
             print ("Getting new data for {} in server {}.".format(user.name, message.server.name))
 
@@ -95,33 +117,44 @@ async def trackmessages(message):
             print ("Getting new data for {} in new server {}.".format(user.name, message.server.name))
             msgData[0].append(sid)
             sindex = msgData[0].index(sid)
+            
             msgData[1].append([[],[]])
             msgData[1][sindex][0].append(uid)
+            
             uindex = msgData[1][sindex][0].index(uid)
-            userData = [[] for i in range(2)]
+            userData = [[],[],[0,datetime.datetime.now()]]
+            
             msgData[1][sindex][1].append(userData)
             newuser = True
         except:
             print ('Something failed lol')
             
-    if newuser == True:        
+    
+    if newuser == True:
         for channel in server.channels:
-            async for log in client.logs_from(channel, limit=100000):
-                if log.author == user:
-                    msgData[1][sindex][1][uindex][1].append([message.channel, message.timestamp])
+            try:
+                async for log in client.logs_from(channel, limit=100000):
+                    if log.author == user:
+                        msgData[1][sindex][1][uindex][1].append([message.channel, message.timestamp])
+            except:
+                print ("Forbidden error. Probably a nsfw channel.")
     else:
+        
         lastmsgtimestamp = msgData[1][sindex][1][uindex][1][-1][1]
         for channel in server.channels:
-            async for log in client.logs_from(channel, limit=100000, after = lastmsgtimestamp):
-                if log.author == user:
-                    msgData[1][sindex][1][uindex][1].append([message.channel, message.timestamp])
-
+            try:
+                async for log in client.logs_from(channel, limit=100000, after = lastmsgtimestamp):
+                    if log.author == user:
+                        msgData[1][sindex][1][uindex][1].append([message.channel, message.timestamp])
+            except:
+                print ("Forbidden error. Probably a nsfw channel.")
+    
     userMessages = len(msgData[1][sindex][1][uindex][1])
+    msgData[1][sindex][1][uindex][2][0] = userMessages
     end_time = datetime.datetime.now()
     uptime = end_time - start_time
     await client.edit_message(tmp, '{} has posted {} messages. Took {} seconds.'.format(user.display_name, userMessages, uptime))
 
-    file_pi = open('msgData.obj', 'wb')
-    pickle.dump(msgData, file_pi) 
+    pickle.dump(msgData, open('msgData.dat', 'wb')) 
 
 client.run(token)
